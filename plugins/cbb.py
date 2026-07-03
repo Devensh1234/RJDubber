9686528199a833eb3ca68eebf608db15afa57ea2
Devensh1234 <133597112+Devensh1234@users.noreply.github.com>
2026-07-03 20:54:38 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ 🖤 Mʏ ᴄʀᴇᴀᴛᴏʀ : <a href='https://t.me/RJ_Dubbers'>Rᴊ Dᴜʙʙᴇʀꜱ</a>\n○ 😺Dᴏɴᴀᴛᴇ ᴜꜱ ~ <a href='https://t.me/RJFilesBot?start=Z2V0LTIxMDQ1NDA1NzAxNDM5'>Cʟɪᴄᴋ Mᴇ</a>\n○ 🔥Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ 🥶sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : <a href='https://t.me/itz_sahil_official'>ᴘʀɪᴠᴀᴛᴇ ᴄᴏᴅᴇ</a></b>",
+            text = f"<b>○ 🖤 Mʏ ᴄʀᴇᴀᴛᴏʀ : <a href='https://t.me/RJ_Dubbers'>Rᴊ Dᴜʙʙᴇʀꜱ</a>\n○ 😺Dᴏɴᴀᴛᴇ ᴜꜱ ~ <a href='https://t.me/RJ_Files_Bot?start=Z2V0LTUyNzEzNzMwNDcxMjIzNA'>Cʟɪᴄᴋ Mᴇ</a>\n○ 🔥Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ 🥶sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : <a href='https://t.me/itz_sahil_official'>ᴘʀɪᴠᴀᴛᴇ ᴄᴏᴅᴇ</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
